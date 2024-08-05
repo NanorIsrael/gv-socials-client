207d8ad33d7c164003aca2359dfdffc359d96c74
nanorisrael <israel.nanor@compozelabs.com>
2024-08-05 13:14:15 +0000

    Add setup files

---
+++ src/state/index.tsx
@@ -0,0 +1,66 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export interface UserI {
+	_id: string;
+	email: string;
+	password: string;
+	lastName: string;
+	firstName: string;
+	friends: Array<string>;
+	occupation: string;
+	lacation: string;
+	photo: string;
+}
+
+interface iState {
+	mode: string,
+	user: null | UserI,
+	token: null | string,
+	posts: Array<{
+		_id: string
+	}>
+};
+
+const initialState: iState = {
+	mode: "light",
+	user: null,
+	token: null,
+	posts: [],
+}
+
+export const authSlice = createSlice({
+	name: "auth",
+	initialState,
+	reducers: {
+		setMode: (state) => {
+			state.mode = state.mode ===  "light" ? "dark" : "light"
+		},
+		setLogin: (state, action) => {
+			state.user = action.payload.user,
+			state.token = action.payload.token
+		},
+		setLogout: (state) => {
+			state.user = null,
+			state.token = null
+		},
+		setFriends: (state, action) => {
+			if (state.user) {
+				state.user.friends = action.payload.friends;
+			} else {
+				console.log("user friends non-existent :(")
+			}
+		},
+		setPosts: (state, action) => {
+			state.posts = action.payload.posts;
+		},
+		setPost: (state, action) => {
+			const updatedPosts = state.posts.map(post => {
+				if (post._id === action.payload.post_id) {
+					return action.payload.post;
+				}
+				return post;
+			})
+			state.posts = updatedPosts
+		}  
+	}
+})
